970683ae5965159bdd6be132f1c547d1ab305a64
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-01-15 20:57:44 +0530

    lecture 11 codes

---
+++ Lecture-11/intro_js/app.js
@@ -0,0 +1,64 @@
+// let abc = 10
+// console.log(abc)
+
+
+// ---------
+
+// let abc = 10; console.log(abc)
+
+// ---------
+
+
+// let a = 10;
+// let b = 20;
+// console.log(a , b);
+// console.log(a);
+// console.log(b);
+
+
+
+// --------------------------------------------------------
+
+// let a = 10;
+// let b = 10.55;
+// let c = -10.65;
+
+// console.log(a , b , c);
+
+// console.log( typeof(a) ); //number
+// console.log( typeof(b) ); //number
+// console.log( typeof(c) ); //number
+
+
+// --------------------------------------------------------
+
+
+// let a = "sam";
+// let b = 'sam';
+// let c = `sam`; //backticks
+
+// console.log(a,b,c);
+
+// console.log( typeof(a) );
+// console.log( typeof(b) );
+// console.log( typeof(c) );
+
+// --------------------------------------------------------
+
+
+// OPERATORS => + ,- , * ,\ ,% ,**
+
+let a = 10.5; //numbers
+let b = 20.5; //numbers
+
+
+// console.log( a + b ); // 10 + 20 = 30
+// console.log( a - b ); // 10 - 20 = -10
+// console.log( a * b ); // 10 * 20 = 200
+// console.log( a / b ); // 10 / 20 = 0.5
+// console.log( a % b ); // 10 % 20 = 10
+// console.log( a ** 2 );// 10 ** 2 = 100
+
+// BODMAS ❌
+// PEMDAS ✅ -> PARENTHESIS , EXPONENTIAL , MULTI , DIVIDE , ADD , SUBTRACT
+// (10+20) * 2 / 5 - (4 * 8)**2
