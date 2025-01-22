3f6855f124686dfee95778c8b48f5ba7720a664e
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-01-22 19:06:12 +0530

    lecture 14 codes

---
+++ Lecture-14/execution-context/app.js
@@ -0,0 +1,90 @@
+
+
+// console.log(a);
+// bro();
+
+// var a = 10;
+
+
+// function bro(){
+//     console.log("i am bro");
+// }
+
+
+// -----------------------------
+
+
+// console.log(a);
+// var a = 10;
+
+// b()
+// function b(){
+//     console.log(c);
+//     var c = 200
+//     console.log(c);
+// }
+
+// console.log(a);
+
+
+
+// -----------------------------
+
+
+
+
+// mav()
+
+// function mav(){
+//     console.log(a);
+//     var a = 10;
+//     console.log(a);
+// }
+
+
+// console.log(sam);
+// var sam = true;
+// console.log(sam);
+
+
+
+// ---------------------------------------------------------------
+
+// hoisting : whenever you try to access a variable or a fn even before its declaration
+// then we say hoisting happens
+
+
+// H1 -> general hoisting              -> var           -> undefined
+// H2 -> dead temporal zone hoisting   -> let and const -> error (special error)
+
+// ---------------------------------------------------------------
+
+// hoisting ❌
+// var a = 20;
+// console.log(a);
+
+// function sam(){
+//     console.log("hello sam");
+// }
+// sam();
+
+// ---------------------------------------------------------------
+
+// hoisting ✅
+console.log(a);
+var a = 20;
+
+sam();
+function sam(){
+    console.log("hello sam");
+}
+
+// ---------------------------------------------------------------
+
+
+
+
+
+
+
+
